0d4836864ec1b73796c27638026926e09c0b8ca6
Alexander Lebedev <lebedev.alex.dev@gmail.com>
2020-07-12 22:19:57 +0300

    added fire animation

---
+++ animations/fire_animation.py
@@ -0,0 +1,32 @@
+import curses
+from utils import sleep
+
+
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await sleep(2)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await sleep(2)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await sleep(1)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import curses
 import random
 import config
 from animations.star_animation import STAR_ANIMATION, draw_star
+from animations.fire_animation import fire
 from utils import shift_animation, get_random_xy, Sleep
 
 
@@ -11,8 +12,9 @@ def main(canvas):
     curses.curs_set(False)
     coroutines = []
     animation_shifter = shift_animation(STAR_ANIMATION)
+    max_y, max_x = canvas.getmaxyx()
 
-    for x, y in get_random_xy(*canvas.getmaxyx()):
+    for x, y in get_random_xy(max_y, max_x):
         coroutines.append(
             draw_star(
                 canvas=canvas,
@@ -23,13 +25,25 @@ def main(canvas):
             )
         )
 
+    # add single fire animation
+    coroutines.append(
+        fire(
+            canvas=canvas,
+            start_row=max_y - 2,
+            start_column= max_x // 2
+        )
+    )
+
     start_event_loop(coroutines, canvas)
 
 
 def start_event_loop(coroutines, canvas):
     while True:
         for c in coroutines:
-            clock: Sleep = c.send(None)
+            try:
+                clock: Sleep = c.send(None)
+            except (StopIteration, RuntimeError):
+                continue
 
         canvas.refresh()
         time.sleep(clock.seconds)
