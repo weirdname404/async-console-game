05def793c8c4135d3bdb197818743dca516d3a9a
Alexander Lebedev <lebedev.alex.dev@gmail.com>
2020-10-31 21:30:14 +0300

    updated config

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 STAR_DENSITY = 0.03
+STARS = '+*.:'
 # in seconds
 TIC_TIMEOUT = 0.1
-STARS = '+*.:'
-SPACESHIP_SPEED = 3
+# in tics
+GARBAGE_SPAWN_COOLDOWN = 10
+# in coordinates
+START_Y_DELTA = 3
+SPACESHIP_MAX_X_SPEED = 10
+SPACESHIP_MAX_Y_SPEED = 10
 LASER_SPEED = 4
+# enables drawing of garbage objects borders
+DEBUG = 0
