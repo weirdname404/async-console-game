3a33b68e98dcafa8fe4d4dc23703527c35732a3a
Alexander Lebedev <lebedev.alex.dev@gmail.com>
2020-10-31 21:39:09 +0300

    updated main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import curses
 import random
 import config
-from animations.garbage.space_garbage import fill_orbit_with_garbage
+from core.events import fill_orbit_with_garbage
 from animations.star_animation import STAR_ANIMATION, animate_star
 from animations.spaceship.animation import (
     animate_spaceship, FRAME_HEIGHT
 )
+from core.controller import run_spaceship
 from core.event_loop import GameLoop
+from core.obstacles import show_obstacles
 from utils import shift_animation
 from utils.curses_tools import get_random_coordinate
 
@@ -18,16 +20,15 @@ def main(canvas):
     max_y, max_x = height - 1, width - 1
     canvas_center = max_x // 2
     spaceship_x = canvas_center
-    spaceship_y = max_y - FRAME_HEIGHT - config.SPACESHIP_SPEED
+    spaceship_y = max_y - FRAME_HEIGHT - config.START_Y_DELTA
 
     animation_shifter = shift_animation(STAR_ANIMATION)
     coroutines = []
-    for x, y in get_random_coordinate(max_y, max_x):
+    for x, y in get_random_coordinate(max_x, max_y):
         coroutines.append(
             animate_star(
                 canvas=canvas,
-                row=y,
-                column=x,
+                pos=(x, y),
                 star=random.choice(config.STARS),
                 animation=next(animation_shifter)
             )
@@ -35,11 +36,14 @@ def main(canvas):
 
     coroutines.extend(
         [
-            # add spaceship
-            animate_spaceship(canvas=canvas, x=spaceship_x, y=spaceship_y),
-            fill_orbit_with_garbage(canvas)
+            animate_spaceship(),
+            run_spaceship(canvas=canvas, x=spaceship_x, y=spaceship_y),
+            fill_orbit_with_garbage(canvas),
         ]
     )
+
+    if config.DEBUG:
+        coroutines.append(show_obstacles(canvas))
 
     GameLoop(coroutines, canvas).start()
 
