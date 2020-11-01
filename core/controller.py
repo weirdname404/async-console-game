f178156d3fde0c1bd4505946cc5e69161e5a8d78
Alexander Lebedev <lebedev.alex.dev@gmail.com>
2020-11-01 23:06:39 +0300

    added game over event

--- core/controller.py
+++ core/controller.py
@@ -1,10 +1,11 @@
 from animations.spaceship.animation import FRAME_HEIGHT, FRAME_WIDTH
 from animations.fire_animation import animate_gunshot
 from animations.explosion import animate_explosion
-from core.objects import SpaceShip
+from core.objects import SpaceShip, Game
 from core.obstacles import obstacle_manager
 from core.physics import update_velocity
 from core.event_loop import Sleep, GameLoop
+from core.events import show_gameover
 from config import SPACESHIP_MAX_X_SPEED, SPACESHIP_MAX_Y_SPEED
 from utils.curses_tools import read_controls, clean_draw, draw_frame
 
@@ -24,6 +25,7 @@ async def run_spaceship(canvas, x, y):
     max_y = height - FRAME_HEIGHT - 1
     prev_frame = None
     game_loop = GameLoop()
+    game = Game()
     ship = SpaceShip()
     vel_x, vel_y = 0, 0
 
@@ -32,7 +34,7 @@ async def run_spaceship(canvas, x, y):
         frame = ship.frame
         # read controls
         dy, dx, space = read_controls(canvas)
-        if space:
+        if space and game.is_gun_available():
             # TODO add fire cooldown
             game_loop.add_coroutine(
                 animate_gunshot(
@@ -58,8 +60,10 @@ async def run_spaceship(canvas, x, y):
 
         # check collisions with obstacles
         for obstacle in obstacle_manager.get_front_objects(x, x + FRAME_WIDTH):
+            # GAME OVER
             if obstacle.has_collision(obj_pos=(x, y),
                                       obj_size=(FRAME_WIDTH, FRAME_HEIGHT)):
+                game.over()
                 # clear last spaceship frame
                 draw_frame(canvas, x, y, frame, True)
                 spaceship_center_pos = (
@@ -69,6 +73,9 @@ async def run_spaceship(canvas, x, y):
                 game_loop.add_coroutine(
                     animate_explosion(canvas=canvas,
                                       center_pos=spaceship_center_pos)
+                )
+                game_loop.add_coroutine(
+                    show_gameover(canvas)
                 )
                 return
 
