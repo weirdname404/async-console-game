adbb6c9055f23122dd125e60fd7033bde8e10f5d
Alexander Lebedev <lebedev.alex.dev@gmail.com>
2020-11-01 23:07:25 +0300

    updated config

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+# enables drawing of garbage objects borders
+DEBUG = False
+# game setup
+START_YEAR = 1955
 STAR_DENSITY = 0.03
 STARS = '+*.:'
 # in seconds
@@ -5,9 +9,9 @@ TIC_TIMEOUT = 0.1
 # in tics
 GARBAGE_SPAWN_COOLDOWN = 10
 # in coordinates
+TEXT_WINDOW_H = 2
+TEXT_WINDOW_W = 55
 START_Y_DELTA = 3
-SPACESHIP_MAX_X_SPEED = 10
-SPACESHIP_MAX_Y_SPEED = 10
+SPACESHIP_MAX_X_SPEED = 5
+SPACESHIP_MAX_Y_SPEED = 5
 LASER_SPEED = 4
-# enables drawing of garbage objects borders
-DEBUG = 0
